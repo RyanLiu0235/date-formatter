3a49d779843a1ec1b7ccfe760901925e72a2d3be
stop2stare <emitremmus0235@163.com>
2018-02-01 14:36:32 +0800

    修复测试用例

--- test.js
+++ test.js
@@ -11,63 +11,58 @@
 var assert = require('assert')
 var formatter = require('./')
 
-var now = new Date(2018, 2, 1, 11, 30, 2)
+var now = new Date(2018, 1, 1, 11, 30, 2) // 2018-1-1 11:30:02
+var _now = now.getTime()
+
+var SECOND = 1000
+var MINUTE = 60 * SECOND
+var HOUR = 60 * MINUTE
+var DAY = 24 * HOUR
 
 describe('时间格式化', function() {
   describe('距离2018-02-01 11:30:02', function() {
     it('1分钟以内，要返回\'刚刚\'', function() {
-      assert.deepStrictEqual(formatter(now.time + 59 * 1000, now), '刚刚')
+      assert.deepStrictEqual(formatter(_now - MINUTE + SECOND, _now), '刚刚')
     })
 
     it('1分钟，要返回\'1分钟前\'', function() {
-      assert.deepStrictEqual(formatter(now.time + 60 * 1000, now), '1分钟前')
+      assert.deepStrictEqual(formatter(_now - MINUTE, _now), '1分钟前')
     })
 
     it('59分钟59秒，要返回\'59分钟前\'', function() {
-      var now = Date.now()
-      assert.deepStrictEqual(formatter(now.time + (59 * 60 + 59) * 1000, now), '59分钟前')
+      assert.deepStrictEqual(formatter(_now - HOUR + SECOND, _now), '59分钟前')
     })
 
     it('1个小时，要返回\'1小时前\'', function() {
-      var now = Date.now()
-      assert.deepStrictEqual(formatter(now.time + 60 * 60 * 1000, now), '1小时前')
+      assert.deepStrictEqual(formatter(_now - HOUR, _now), '1小时前')
     })
 
     it('23个小时59分钟59秒，要返回\'23小时前\'', function() {
-      var now = Date.now()
-      assert.deepStrictEqual(formatter(now.time + (23 * 60 * 60 + 59 * 60 + 59) * 1000, now), '23小时前')
+      assert.deepStrictEqual(formatter(_now - DAY + SECOND, _now), '23小时前')
     })
 
     it('24个小时，要返回\'昨天\'', function() {
-      var now = Date.now()
-      console.log('24个小时前：', new Date(now.time + 24 * 60 * 60 * 1000))
-      assert.deepStrictEqual(formatter(now.time + 24 * 60 * 60 * 1000, now), '昨天')
+      assert.deepStrictEqual(formatter(_now - DAY, _now), '昨天')
     })
 
     it('昨天零点，要返回\'昨天\'', function() {
-      var then = new Date(2018, 1, 31)
-      assert.deepStrictEqual(formatter(then, now), '昨天')
+      var then = new Date(2018, 0, 31)
+      assert.deepStrictEqual(formatter(then, _now), '昨天')
     })
 
-    it('昨天零点前一秒，要返回\'1月30日\'', function() {
-      var then = new Date(2018, 1, 30, 23, 59, 59)
-
-      console.log('then', new Date(then))
-      assert.deepStrictEqual(formatter(then, now), '1月30日')
+    it('昨天零点前一秒，要返回\'1-30\'', function() {
+      var then = new Date(2018, 0, 30, 23, 59, 59)
+      assert.deepStrictEqual(formatter(then, _now), '1-30')
     })
 
-    it('2018-01-01 00:00:00，要返回\'1月1日\'', function() {
-      var then = new Date(2018, 1, 1)
-
-      console.log('then', new Date(then))
-      assert.deepStrictEqual(formatter(then, now), '1月1日')
+    it('2018-01-01 00:00:00，要返回\'1-1\'', function() {
+      var then = new Date(2018, 0, 1)
+      assert.deepStrictEqual(formatter(then, _now), '1-1')
     })
 
-    it('2017-12-31 23:59:59，要返回\'2017年12月31日\'', function() {
-      var then = new Date(2017, 12, 31)
-
-      console.log('then', new Date(then))
-      assert.deepStrictEqual(formatter(then, now), '2017年12月31日')
+    it('2017-12-31 23:59:59，要返回\'2017-12-31\'', function() {
+      var then = new Date(2017, 11, 31)
+      assert.deepStrictEqual(formatter(then, _now), '2017-12-31')
     })
   })
 })
