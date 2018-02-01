86f833239ca0d157d8185135f49d26006df84433
stop2stare <emitremmus0235@163.com>
2018-02-01 14:36:49 +0800

    实现格式化函数

--- index.js
+++ index.js
@@ -8,10 +8,37 @@
  * 大于自然日1月1日0点 —— 年-月-日（例: 2016-8-5
  */
 
-var formatter = function(then, now) {
-  var now = now === undefined ? new Date() : now
+/**
+ * 时间格式化
+ *
+ * @param 	{timeString} 						then 	一个遥远的时间
+ * @param 	{timeString|undefined} 	now 	当前的时间，不传默认为当前
+ * @return 	{String} 					按照上面的规则来返回
+ */
 
-  return ''
+var MINUTE = 60 * 1000
+var HOUR = 60 * MINUTE
+var DAY = 24 * HOUR
+
+var formatter = function(_then, _now) {
+  _now = _now === undefined ? Date.now() : _now
+  var now = new Date(_now)
+  var then = new Date(_then)
+  var distance = _now - _then
+
+  if (distance < MINUTE) {
+    return '刚刚'
+  } else if (distance < HOUR) {
+    return Math.floor(distance / MINUTE) + '分钟前'
+  } else if (distance < DAY) {
+    return Math.floor(distance / HOUR) + '小时前'
+  } else if (_then >= new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1).getTime()) {
+    return '昨天'
+  } else if (_then >= new Date(now.getFullYear(), 0, 1).getTime()) {
+    return then.getMonth() + 1 + '-' + then.getDate()
+  } else {
+    return then.getFullYear() + '-' + (then.getMonth() + 1) + '-' + then.getDate()
+  }
 }
 
 module.exports = formatter
